3420d23ef6d10b14196f452a7220baa1c7bc4f69
Renan Maidana <renanmaidana@gmail.com>
2016-11-21 12:17:33 -0200

    Changed detector_firm.ino

--- eletronics/arduino_firmware/detector_firm.ino
+++ eletronics/arduino_firmware/detector_firm.ino
@@ -1,26 +1,11 @@
-#define USB_CON
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#include <ros.h>
-#include <std_msgs/String.h>
-#include <metal_detector/Coil.h>
-
 volatile double temp;
-volatile int32_t freq1 = 0;
-volatile int32_t freq2 = 0;
+volatile int freq1 = 0;
+volatile int freq2 = 0;
 long int swit = 0;
 bool Rstate = 0;
-
-//ROS handler
-ros::NodeHandle nh;
-
-//ROS messages
-metal_detector::Coil coil_msg;
-
-//ROS publishers and subscribers
-ros::Publisher detector("metal_detector", &coil_msg);
 
 //Interrupt vector for INT0 (pin D2)
 ISR (INT0_vect)
@@ -30,11 +15,11 @@ ISR (INT0_vect)
     if(Rstate == true){
       freq1 = (1/((micros() - temp)/1000000));
   	  freq2 = 0;
-    }
+  	}
     else{
       freq2 = (1/((micros() - temp)/1000000));
       freq1 = 0;
-    }
+  	}
   }
   temp = micros();
 }
@@ -43,15 +28,6 @@ void setup()
 {
   pinMode(13,OUTPUT);	//Relay pin (D13)
   temp = micros();
-
-  //Initialize ROS node
-  nh.initNode();
-  nh.advertise(detector);
-
-  //Fill out the static parameters of the coil message type
-  coil_msg.header.frame_id = "Coil";
-  coil_msg.header.seq = 0;
-  coil_msg.channel_length = 2;
   
   cli(); //Disable interrupts for setup
 
@@ -64,26 +40,21 @@ void setup()
   sei();	//Re-enable interrupts
 }
 
-//Local vector to be passed to hold frequencies to be passed as pointers to the coil message
-int32_t freqs[2];
-
 void loop()
 {
   //After some time, switch coils
-  if(swit >= 20)
+  if(swit >= 80000)
   { 
     Rstate = not(Rstate); 
     swit = 0;
     digitalWrite(13,Rstate);
   }
-  swit++;
+  
+  //Send frequencies through serial as soon as possible
+  Serial.print(freq1);
+  Serial.print(",");
+  Serial.print(freq2);
+  Serial.print("\n");
 
-  //Populate message and publish it aprox every 100 ms (10 Hz)
-  coil_msg.header.stamp = nh.now();
-  freqs[0] = freq1; 
-  freqs[1] = freq2;
-  coil_msg.channel = freqs;
-  detector.publish(&coil_msg);
-  nh.spinOnce();
-  delay(100);
+  swit++;
 }
